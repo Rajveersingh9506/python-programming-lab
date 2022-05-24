42f4c9f106ea966a0cc8f3b79a84fe6c66af8866
Rajveersingh9506 <106173413+Rajveersingh9506@users.noreply.github.com>
2022-05-24 21:53:53 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,6 @@
+x=int(input("enter first number-->"))
+y=int(input("enter second number-->"))
+area=x*y
+perimeter=2*(x+y)
+print("area of rectangle is-->",area)
+print("perimeter of rectangle is-->",perimeter)
