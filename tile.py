c66b164e2f835af1b6a2fd623ccb68b466cd7caf
Rajveersingh9506 <106173413+Rajveersingh9506@users.noreply.github.com>
2022-05-24 22:31:48 +0530

    Add files via upload

---
+++ tile.py
@@ -0,0 +1,10 @@
+l1=int(input("enter length of room-->"))
+b1=int(input("enter the breadth of room-->"))
+area1=l1*b1
+l2=int(input("enter length of tile-->"))
+b2=int(input("enter the breadth of tile-->"))
+area2=l2*b2
+tile=area1/area2
+print("number of tile",tile)
+
+
